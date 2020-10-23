1e4295f25f79c8b381f6eba2c0a3eb315fa9cc66
dmmaia <david_mmaia@hotmail.com>
2020-10-22 21:50:06 -0300

    Creating component to convert US to R

---
+++ src/components/Converter.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="converter-container">
+  <div><h2>US$:</h2><input v-model="moedaA_value" type="text" /></div>
+  <h2>R$: {{moedaB_value}}</h2>
+  <button v-on:click="converter">Converter</button>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'Converter',
+
+    data(){
+      return {
+        moedaA_value: "",
+        moedaB_value: 0
+      };
+    },
+
+    methods: {
+      converter() {
+        let url = "https://free.currconv.com/api/v7/convert?q=USD_BRL&compact=ultra&apiKey=590a41dfbd78a12428b8";
+
+        fetch(url).then(res => {return res.json()})
+        .then(json=>{
+          let cotacao = json["USD_BRL"];
+          this.moedaB_value = (cotacao * parseFloat(this.moedaA_value))
+          .toFixed(2)
+          console.log(cotacao)
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  .converter-container {
+    display: flex;
+    flex-direction: column;
+    max-width: 200px;
+    align-items: center;
+    justify-content: center;
+  }
+  .converter-container div{
+    flex-direction: row;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .converter-container div input {
+    margin-left: 8px;
+    font-size: 20px;
+    max-width: 150px;
+}
+</style>
